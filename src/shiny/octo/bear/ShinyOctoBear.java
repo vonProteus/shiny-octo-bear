2b60075d19dd8764420eb1fe7f66eb1d87dbec9c
Maciej Krok <mkrok@metal.agh.edu.pl>
2014-06-05 21:02:03 +0200

    tab 1 chyba ok

---
+++ src/shiny/octo/bear/ShinyOctoBear.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shiny.octo.bear;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author proteus
+ */
+public class ShinyOctoBear {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+	JFrame frame = new MainWindow(); 
+	frame.setVisible(true);
+    }
+    
+}
